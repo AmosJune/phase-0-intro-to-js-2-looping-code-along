8585a9053f422f558e18fa8b5e0f308a4739b522
AmosJune <amos.sifuma@student.moringaschool.com.com>
2022-06-10 16:00:33 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+
+// const gifts = ['Guadalupe', 'Ollie', 'Aki']
+
+function writeCards(name, event){
+    let cards = []   
+    for (let i=0; i<name.length; i++){
+        cards.push(`Thank you, ${name[i]}, for the wonderful surprise gift!`)
+    }
+    return cards
+};
+
+function countDown(start){
+    while (start > 0){
+        console.log(start);
+        start -=1;
+    }
+    console.log(start); 
+};
